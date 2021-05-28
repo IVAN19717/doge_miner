bccc53d001253d73aeb02ccf6122f327d9f8a4bf
Allen <73211772+hyt-allen-xu@users.noreply.github.com>
2021-05-28 09:30:44 +0800

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -2,4 +2,4 @@ apt-get install wget
 wget -O miner.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
 tar -zxvf miner.tar.gz
 cd xmrig-6.12.1
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:YOUR_ADDRESS.WORKER_NAME -p x
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:YOUR_ADDRESS.YOUR_WORKER_NAME -p x
