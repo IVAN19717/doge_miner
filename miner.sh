e4bb3b56ef84ce7273a17183f344c9f6eef248a3
Allen <73211772+hyt-allen-xu@users.noreply.github.com>
2021-05-30 20:46:59 +0800

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,6 +1,6 @@
 apt-get install wget
 apt-get install screen
-wget -O miner.tar.gz https://github.com/xmrig/xmrig/releases/latest/download/xmrig-6.12.1-linux-x64.tar.gz
+wget -O miner.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
 tar -zxvf miner.tar.gz
 cd xmrig*
 screen -S miner
