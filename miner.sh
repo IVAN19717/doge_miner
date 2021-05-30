1dcb052b032ed169c85da14b45592ba93cca26a5
Allen <73211772+hyt-allen-xu@users.noreply.github.com>
2021-05-30 17:32:39 +0800

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,7 +1,7 @@
 apt-get install wget
 apt-get install screen
-wget -O miner.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+wget -O miner.tar.gz https://github.com/xmrig/xmrig/releases/latest/download/xmrig-6.12.1-linux-x64.tar.gz
 tar -zxvf miner.tar.gz
-cd xmrig-6.12.1
+cd xmrig*
 screen -S miner
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:YOUR_ADDRESS.YOUR_WORKER_NAME#referral_code(optional) -p x
