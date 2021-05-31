5fa6bbdfe252ef52b361ab321fffe7a20dd66c86
Allen <73211772+hyt-allen-xu@users.noreply.github.com>
2021-05-31 09:04:36 +0800

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,5 +1,15 @@
-apt-get install wget
-apt-get install screen
+os=`cat /etc/redhat-release`
+if [[ "${release}" == "ubuntu" || "${release}" == "debian" ]]; then
+    if [[ ${bit} = "x86_64" ]]; then
+        apt-get install wget
+        apt-get install screen
+    fi
+else
+    yum -y install wget
+    yum -y install screen
+fi
+
+
 wget -O miner.tar.gz https://github.com/hyt-allen-xu/linux/releases/download/xmrig/xmrig-linux-x64.tar.gz
 tar -zxvf miner.tar.gz
 cd xmrig*
