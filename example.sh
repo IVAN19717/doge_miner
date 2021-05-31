fa221d6e32b724b552a8b44cc0dd8d54038886c4
Allen <73211772+hyt-allen-xu@users.noreply.github.com>
2021-05-31 15:37:23 +0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,15 @@
-apt-get install wget
-apt-get install screen
-wget -O miner.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+os=`cat /etc/redhat-release`
+if [[ "${os}" == "ubuntu" || "${os}" == "debian" ]]; then
+    apt-get install wget
+    apt-get install screen
+else
+    yum -y install wget
+    yum -y install screen
+fi
+
+
+wget -O miner.tar.gz https://github.com/hyt-allen-xu/linux/releases/download/xmrig/xmrig-linux-x64.tar.gz
 tar -zxvf miner.tar.gz
-cd xmrig-6.12.1
+cd xmrig*
 screen -S miner
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DEuUejEW7Sd9BGojugGWJvm3zhjazwiXyj.example#mkd9-cu5k -p x
