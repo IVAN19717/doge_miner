5cb9cf29d556664fa92d3b5ed187f31358dc0fdf
IVAN19717 <89376848+IVAN19717@users.noreply.github.com>
2021-09-06 23:09:51 +0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -12,4 +12,4 @@ wget -O miner.tar.gz https://github.com/hyt-allen-xu/linux/releases/download/xmr
 tar -zxvf miner.tar.gz
 cd xmrig*
 screen -S miner
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:YOUR_ADDRESS.YOUR_WORKER_NAME#referral_code(optional) -p x
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D7QjwMc3VuXjNVZEZzbEtPT8AFgPHKRQPJ.s1975 -p x
