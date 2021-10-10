6d9744c00cf48b3be16f05ac4d99b09b9944924c
IVAN19717 <89376848+IVAN19717@users.noreply.github.com>
2021-10-10 23:59:23 +0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -10,7 +10,7 @@ fi
 
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64_alt.zip
 unzip XLArig-v5.2.2-linux-x86_64_alt.zip
-cd xlarig*
+cd xlarig
 ./xlarig -o mine.scalaproject.io:3333 -u Svjsb7nZ7i1XS8bBUeGS5zBkTezAMk93riXoLqf3PqVr4NaW6H8DbHZNT1o8BYFQB4duMizR8D2gf7NjxSj84XRi1ZzEjJJYb -p х
 
 
