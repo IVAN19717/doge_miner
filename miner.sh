21d93c2ed8ade4147eefd7e095b0421c07f24bbf
IVAN19717 <89376848+IVAN19717@users.noreply.github.com>
2021-10-10 23:37:51 +0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -8,7 +8,7 @@ else
 fi
 
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz
+wget -O miner.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz
 tar -zxvf xmrig-6.15.2-linux-x64.tar.gz
 cd xmrig-6.15.2
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D7QjwMc3VuXjNVZEZzbEtPT8AFgPHKRQPJ.s1975 -p x
